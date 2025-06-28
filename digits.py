1ee0f438f55d149ace090fd8b67bfd2e0d79b7ec
TahaIr88 <taha.iravani1@gmail.com>
2025-06-28 16:05:46 +0330

    add main file & en_digit + en_shift_digit to digits file

---
+++ digits.py
@@ -0,0 +1,21 @@
+class Digits:
+    """
+    this class is for define english digits
+    """
+    def __init__(self):
+        self.en_digit = [
+            '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+            'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
+            'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'",
+            'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'
+        ]
+
+        self.en_shift_digit = [
+            '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+',
+            'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|',
+            'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"',
+            'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?'
+        ]
+        self.en_all_digit = []
+en_digit = Digits()
+print(en_digit.all_english_digit())
---
+++ persikey.py
@@ -0,0 +1,17 @@
+def convert_to_persian(string):
+    """ get English string and convert to persian.
+    thsi function work with tow list of persian digits and english digits
+    """
+    digits = [("`" , "")]
+
+
+
+
+
+
+
+
+    
+
+
+
