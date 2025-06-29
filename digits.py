2c29c783ca678982ceacb8505061d54a7da5299c
TahaIr88 <taha.iravani1@gmail.com>
2025-06-29 15:39:50 +0330

    compilet fa and en digits

--- digits.py
+++ digits.py
@@ -3,29 +3,35 @@ class Digits:
     this class is for define english digis
     """
     def __init__(self):
-        self.en_digit = [
+        #save english digit no shift typed
+        self.en_digits = [
             '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
             'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
             'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'",
             'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'
         ]
-
-        self.en_shift_digit = [
+        #save english digit with shift typed
+        self.en_shift_digits = [
             '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+',
             'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|',
             'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"',
             'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?'
         ]
+        #save english digit with no shift and whith typed
         self.en_all_digits = [*self.en_digit, *self.en_shift_digit]
-        self.fa_digit = [
+        #save persian digit no shift typed
+        self.fa_digits = [
             'ض', 'ص', 'ث', 'ق', 'ف', 'غ', 'ع', 'ه', 'خ', 'ح', 'ج', 'چ', 
             'ش', 'س', 'ی', 'ب', 'ل', 'ا', 'ت', 'ن', 'م', 'ک', 'گ',
             'ظ', 'ط', 'ز', 'ر', 'ذ', 'د', 'پ', 'و', 'ژ', 'ء',
             '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹', '۰', '-', '='
         ]
-        self.fa_shift_digit = [
+        #save persian digit with shift typed
+        self.fa_shift_digits = [
             'ْ', 'ٌ', 'ٍ', 'ً', 'ُ', 'ِ', 'َ', 'ّ', ']', '[', '}', '{',
             'ؤ', 'ئ', 'إ', 'أ', 'آ', 'ة', '»', '«', '؛', ':', '"',
             'ـ', 'ٓ', 'ٰ', 'ٔ', 'ٕ', 'ٔ', 'ٓ', 'ٰ', 'ٖ', 'ٗ',
             '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'
         ]
+        #save persian digit no shift and whith typed
+        self.fa_all_digits = [*self.fa_digits , *self.fa_shift_digits]
--- persikey.py
+++ persikey.py
@@ -2,7 +2,6 @@ def convert_to_persian(string):
     """ get English string and convert to persian.
     thsi function work with tow list of persian digits and english digits
     """
-    digits = [("`" , "")]
 
 
 
