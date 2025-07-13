9fcafa66f5577b21d523cb207101e71beafc9038
TahaIr88 <taha.iravani1@gmail.com>
2025-07-13 15:44:22 +0330

    add get text function and set text function to selected text file

--- mudel/keyboard/selected_text.py
+++ mudel/keyboard/selected_text.py
@@ -7,22 +7,19 @@ def get_selected_text():
     keyboard = Controller()
     keyboard.press(Key.ctrl)
     keyboard.press('c')
-    time.sleep(0.52)
+    #time.sleep(0.52)
     keyboard.release(Key.ctrl)
     keyboard.release('c')
-    time.sleep(0.52)
+    #time.sleep(0.52)
 
     return pyperclip.paste() #return into clipboard
 
 def set_selected_text(text):
     pyperclip.copy(text)
-    time.sleep(0.52)
+    #time.sleep(0.52)
     keyboard = Controller()
     keyboard.press(Key.ctrl)
     keyboard.press('v')
-    time.sleep(0.52)
+    #time.sleep(0.52)
     keyboard.release(Key.ctrl)
     keyboard.release('v')
-
-time.sleep(5)
-set_selected_text("in yek matn copy shode asttttt  ) : ")
